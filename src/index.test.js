c599ee04ea94523163f41968506bb3110c4bb31a
Jerry Hu <hh201020@gmail.com>
2017-01-29 23:25:42 -0500

    Centralizing HTTP Requests & Fetch approach vanilla javascript HTTP call

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 })
 
 describe('index.html', () => {
-  it('should have h1 that says hello WiW', (done) => {
+  it('should have h1 that says hello users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello WiW");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
