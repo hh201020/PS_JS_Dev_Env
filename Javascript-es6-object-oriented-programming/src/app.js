be242fa0bd656e51cefe4d87df15e1504ef9b874
Jerry Hu <hh201020@gmail.com>
2017-02-02 14:17:07 -0500

    load titlebar

--- Javascript-es6-object-oriented-programming/src/app.js
+++ Javascript-es6-object-oriented-programming/src/app.js
@@ -5,9 +5,11 @@ import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
 import {Button} from './ui/button.js';
 import {Image} from './ui/image.js';
+import {TitleBar} from './ui/title-bar.js';
 
-let b = new Button('Click Me');
-b.appendToElement($('body'));
-
-let i = new Image('../images/drone.jpg');
-i.appendToElement($('body'));
+let tb = new TitleBar('Our Application');
+tb.addLink('Home', '');
+tb.addLink('Cars', '');
+tb.addLink('Drones', '');
+tb.addLink('Map', '');
+tb.appendToElement($('body'));
