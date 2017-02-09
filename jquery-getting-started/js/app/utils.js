866555432c626c1c56293ba783b483daf511acf1
Jerry Hu <hh201020@gmail.com>
2017-02-09 18:59:22 -0500

    does not work well

--- jquery-getting-started/js/app/utils.js
+++ jquery-getting-started/js/app/utils.js
@@ -1,20 +1,20 @@
 (function (window, $) {
-    
+
     window.util = {
-        
+
         getDateString: function(){
             var dt = new Date();
             var month = (dt.getMonth() + 1);
             var day = dt.getDate();
-            
+
             month = month > 10 ? month : '0' + month;
             day = day > 10 ? day : '0' + day;
-            
+
             return dt.getFullYear() + '-' + month + '-' + day;
         },
-        
+
         renderMarkup : function(sourceSelector, destinationSelector, callback){
-            var 
+            var
                 $source = $(sourceSelector),
                 $destination = $(destinationSelector);
 
@@ -23,16 +23,16 @@
                 callback($source, $destination);
             }
         },
-        
+
         replaceBrackets : function(markup){
             markup = markup.replace(/\</g, '&lt;');
             markup = markup.replace(/\>/g, '&gt;');
             return markup;
         }
     };
-    
+
     $(function(){
-        var 
+        var
             $codeContainer = $('#code-container'),
             $codeBlock = $('#code-block');
 
@@ -41,13 +41,23 @@
             var src = $codeBlock.html().replace('\n','');
             $codeContainer.html(window.util.replaceBrackets(src));
         }
-        
+
         $('pre code').each(function(i, block) {
             hljs.highlightBlock(block);
         });
-        
+
         window.util.renderMarkup('#demo-content-container', '#demo-markup', function(src, destination){
             hljs.highlightBlock(destination[0]);
         });
+
+        var items = $('#special-features li');
+        items.width('200%')
+             .height('200px')
+             .addClass('highlight bordered')
+             .each(function(index, item) {
+               var $item = $(item);
+               $item.text($item.text() + ' ' + $item.attr('data-feature-id'));
+             });
+
     });
 }(window, jQuery));
