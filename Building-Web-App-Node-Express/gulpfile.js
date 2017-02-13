7d403547efbdfb9d239931bb964574d3894a3d51
Jerry Hu <hh201020@gmail.com>
2017-02-13 01:09:06 -0500

    Setting up Wiredep to copy links from bower automatically

--- Building-Web-App-Node-Express/gulpfile.js
+++ Building-Web-App-Node-Express/gulpfile.js
@@ -12,3 +12,30 @@ gulp.task('style', function () {
         }))
         .pipe(jscs());
 });
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {
+        read: false
+    });
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+
+});
+
