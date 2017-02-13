f5e75139f133a4f06f2d48de6feb58035d48016b
Jerry Hu <hh201020@gmail.com>
2017-02-13 10:49:11 -0500

    add navigation links

--- Building-Web-App-Node-Express/app.js
+++ Building-Web-App-Node-Express/app.js
@@ -30,9 +30,19 @@ app.use(express.static('public'));
 
 app.set('views', './src/views');
 app.set('view engine', '.ejs');
-app.get('/', function(req, res){
-    res.render('index', {title: 'Hello from render', list:['a', 'b']});
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
+
 
 app.get('/books', function(req, res){
     res.send('Hello Books');
