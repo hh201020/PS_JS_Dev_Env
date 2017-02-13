e5d493e801681a0222ccad86f05d709e76b0f6c2
Jerry Hu <hh201020@gmail.com>
2017-02-13 00:47:06 -0500

    fix jslint problem found in running 'gulp style'

--- Building-Web-App-Node-Express/app.js
+++ Building-Web-App-Node-Express/app.js
@@ -16,5 +16,5 @@ app.get('/books', function(req, res){
 });
 
 app.listen(port, function(err){
-  console.log('running server on port ' + port)
+  console.log('running server on port ' + port);
 });
