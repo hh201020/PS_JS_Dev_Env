cda814178f5ea77470da9fb86b35b951e968300b
Jerry Hu <hh201020@gmail.com>
2017-02-13 10:24:37 -0500

    Template Engine: EJS

--- Building-Web-App-Node-Express/app.js
+++ Building-Web-App-Node-Express/app.js
@@ -5,23 +5,33 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+
+// basic html setting:
 // app.use(express.static('src/views'));
 // app.get('/', function(req, res){
 //     res.send('Hello World');
 // });
 
+// Template engine Jade setting:
 // app.set('views', './src/views');
 // app.set('view engine', 'jade');
 // app.get('/', function(req, res){
 //     res.render('index');
 // });
 
+// Template engine Handlebars setting:
+// app.set('views', './src/views');
+// var handlebars = require('express-handlebars');
+// app.engine('.hbs', handlebars({extname: '.hbs'}));
+// app.set('view engine', '.hbs');
+// app.get('/', function(req, res){
+//     res.render('index');
+// });
+
 app.set('views', './src/views');
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.set('view engine', '.ejs');
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {title: 'Hello from render', list:['a', 'b']});
 });
 
 app.get('/books', function(req, res){
