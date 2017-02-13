054bb1fec1f008ce9531e41007ac49d3356d1957
Jerry Hu <hh201020@gmail.com>
2017-02-13 09:34:34 -0500

    gulp-nodemon to watch js changes to restart server

--- Building-Web-App-Node-Express/gulpfile.js
+++ Building-Web-App-Node-Express/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -30,12 +31,26 @@ gulp.task('inject', function () {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
-
 });
 
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 2222
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting....');
+        });
+});
