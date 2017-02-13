a21f20c81c323300ae8d3680b7ca74412427be3b
Jerry Hu <hh201020@gmail.com>
2017-02-13 13:32:49 -0500

    select one book using mongoldb

--- Building-Web-App-Node-Express/src/routes/bookRoutes.js
+++ Building-Web-App-Node-Express/src/routes/bookRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
 
@@ -23,15 +24,26 @@ var router = function (nav) {
 
     });
 
-  bookRouter.route('/:id')
-    .get(function (req, res) {
-      var id = req.params.id;
-      res.render('bookView', {
-        title: 'Books',
-        nav: nav,
-        book: books[id]
-      });
-    });
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = new objectId(req.params.id);
+            var url =
+                'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+
+                collection.findOne({_id: id},
+                    function (err, results) {
+                        res.render('bookView', {
+                            title: 'Books',
+                            nav: nav,
+                            book: results
+                        });
+                    }
+                );
+            });
+        });
 
   return bookRouter;
 };
