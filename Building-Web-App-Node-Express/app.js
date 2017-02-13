fbba7063556ceec27a14da4d73f40e9593967583
Jerry Hu <hh201020@gmail.com>
2017-02-13 12:03:27 -0500

    make it route to http://localhost:2222/books and http://localhost:2222/books/3 , but http://localhost:2222/ has no links inside.

--- Building-Web-App-Node-Express/app.js
+++ Building-Web-App-Node-Express/app.js
@@ -3,33 +3,23 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+    }, {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
-
-// basic html setting:
-// app.use(express.static('src/views'));
-// app.get('/', function(req, res){
-//     res.send('Hello World');
-// });
-
-// Template engine Jade setting:
-// app.set('views', './src/views');
-// app.set('view engine', 'jade');
-// app.get('/', function(req, res){
-//     res.render('index');
-// });
-
-// Template engine Handlebars setting:
-// app.set('views', './src/views');
-// var handlebars = require('express-handlebars');
-// app.engine('.hbs', handlebars({extname: '.hbs'}));
-// app.set('view engine', '.hbs');
-// app.get('/', function(req, res){
-//     res.render('index');
-// });
-
 app.set('views', './src/views');
-app.set('view engine', '.ejs');
+
+app.set('view engine', 'ejs');
+
+
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
     res.render('index', {
         title: 'Hello from render',
@@ -43,11 +33,10 @@ app.get('/', function (req, res) {
     });
 });
 
-
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
     res.send('Hello Books');
 });
 
-app.listen(port, function(err){
-  console.log('running server on port ' + port);
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
