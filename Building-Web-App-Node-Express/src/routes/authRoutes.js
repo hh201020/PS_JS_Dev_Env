2e342d4503a0b5dfea1d6c202688cccde4520cd1
Jerry Hu <hh201020@gmail.com>
2017-02-16 13:44:51 -0500

    Saving the user into mongodb, the profile will be like: {"username":"bbb","password":"vvv","_id":"58a5f22f4e5f74162be15f90"}

--- Building-Web-App-Node-Express/src/routes/authRoutes.js
+++ Building-Web-App-Node-Express/src/routes/authRoutes.js
@@ -2,13 +2,29 @@ var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
+// when signing up, it calls req.login with req.body,
+// it just adds this user object to this session, and
+// then Passport called its serializeUser or deserializeUser to make it work
 var router = function () {
   authRouter.route('/signUp')
     .post(function (req, res) {
+
       console.log(req.body);
-      req.login(req.body, function () {     // when signing up, it calls req.login with req.body,
-        res.redirect('/auth/profile');      // it just adds this user object to this session, and
-      });                                   // then Passport called its serializeUser or deserializeUser to make it work
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function (err, db) {
+        var collection = db.collection('users');
+        var user = {
+          username: req.body.userName,
+          password: req.body.password
+        };
+
+        collection.insert(user,
+          function (err, results) {
+            req.login(results.ops[0], function () {
+              res.redirect('/auth/profile');
+            });
+          });
+      });
     });
 
   authRouter.route('/profile')
